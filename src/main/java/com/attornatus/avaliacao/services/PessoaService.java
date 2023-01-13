a14c4fbfbba913cf1fbe39b03726432be9c1bdd9
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 00:21:41 -0300

    create exception personalizada ResourceNotFoundException para regra de serviçes e create metodo findById

--- src/main/java/com/attornatus/avaliacao/services/PessoaService.java
+++ src/main/java/com/attornatus/avaliacao/services/PessoaService.java
@@ -1,12 +1,14 @@
 package com.attornatus.avaliacao.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.attornatus.avaliacao.entities.Pessoa;
 import com.attornatus.avaliacao.repositories.PessoaRepository;
+import com.attornatus.avaliacao.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class PessoaService {
@@ -15,6 +17,11 @@ public class PessoaService {
 
 	public List<Pessoa> findAll() {
 		return repository.findAll();
+	}
+	
+	public Pessoa findById(Long id) {
+		Optional<Pessoa> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 }
---
+++ src/main/java/com/attornatus/avaliacao/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.attornatus.avaliacao.services.exceptions;
+
+public class ResourceNotFoundException  extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Recurso não encontrado. id: " + id);
+	}
+
+}
