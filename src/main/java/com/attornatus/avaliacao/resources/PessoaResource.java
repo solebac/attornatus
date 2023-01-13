fa64d52ccffa06020db9f44ff466f59996a020ec
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 00:23:33 -0300

    create end-point findById PESSOA

--- src/main/java/com/attornatus/avaliacao/resources/PessoaResource.java
+++ src/main/java/com/attornatus/avaliacao/resources/PessoaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,12 @@ public class PessoaResource {
 	public ResponseEntity<List<PessoaDto>> findAll(){
 		List<Pessoa> p = services.findAll();
 		return ResponseEntity.ok().body(PessoaDto.toListPessoa(p));
+	}
+	
+	@GetMapping("/pessoas/{id}")
+	public ResponseEntity<PessoaDto> findById(@PathVariable Long id){
+		Pessoa obj = services.findById(id);
+		return ResponseEntity.ok().body(new PessoaDto(obj));
 	}
 
 }
