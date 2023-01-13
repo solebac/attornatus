5c32ea9c823b2e4e72b657b4b4879e7b55d0a4b7
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 00:30:36 -0300

    Finalização do CRUD service Pessoa

--- src/main/java/com/attornatus/avaliacao/services/PessoaService.java
+++ src/main/java/com/attornatus/avaliacao/services/PessoaService.java
@@ -3,11 +3,17 @@ package com.attornatus.avaliacao.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.attornatus.avaliacao.entities.Pessoa;
 import com.attornatus.avaliacao.repositories.PessoaRepository;
+import com.attornatus.avaliacao.services.exceptions.DatabaseExceptionOwn;
 import com.attornatus.avaliacao.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -22,6 +28,39 @@ public class PessoaService {
 	public Pessoa findById(Long id) {
 		Optional<Pessoa> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	@Transactional
+	public Pessoa insert(Pessoa obj) {
+		return repository.save(obj);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseExceptionOwn(e.getMessage());
+		}
+	}
+
+	@Transactional
+	public Pessoa update(Long id, Pessoa obj) {
+
+		try {
+			Pessoa entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Pessoa entity, Pessoa obj) {
+		entity.setNome(obj.getNome());
+		entity.setDataDeNascimento(obj.getDataDeNascimento());
 	}
 
 }
