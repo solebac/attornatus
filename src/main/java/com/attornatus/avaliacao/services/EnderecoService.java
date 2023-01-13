277c8e601c1e2644a335b4cd29898f6da6d3f94b
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 02:30:02 -0300

    implements methods findAll e findById EnderecoService

--- src/main/java/com/attornatus/avaliacao/services/EnderecoService.java
+++ src/main/java/com/attornatus/avaliacao/services/EnderecoService.java
@@ -1,8 +1,27 @@
 package com.attornatus.avaliacao.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.attornatus.avaliacao.entities.Endereco;
+import com.attornatus.avaliacao.repositories.EnderecoRepository;
+import com.attornatus.avaliacao.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EnderecoService {
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
+	public List<Endereco> findAll() {
+		return enderecoRepository.findAll();
+	}
+
+	public Endereco findById(Long id) {
+		Optional<Endereco> obj = enderecoRepository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
 }
