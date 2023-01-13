d076589cbd6534e3d8a0e6f3646ccf33b4dbb87b
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 02:52:02 -0300

    concluido CRUD Endereço

--- src/main/java/com/attornatus/avaliacao/resources/EnderecoResource.java
+++ src/main/java/com/attornatus/avaliacao/resources/EnderecoResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,12 @@ public class EnderecoResource {
 				.path("/{id}").buildAndExpand(obj.getId())
 				.toUri();
 		return ResponseEntity.created(uri).body(new EnderecoDto(obj)); 
+	}
+	
+	@DeleteMapping("/enderecos/{id}") 
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/attornatus/avaliacao/services/EnderecoService.java
+++ src/main/java/com/attornatus/avaliacao/services/EnderecoService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.attornatus.avaliacao.entities.Endereco;
@@ -17,13 +20,14 @@ import com.attornatus.avaliacao.repositories.EnderecoRepository;
 import com.attornatus.avaliacao.repositories.PessoaRepository;
 import com.attornatus.avaliacao.resources.dto.EnderecoDto;
 import com.attornatus.avaliacao.resources.form.EnderecoForm;
+import com.attornatus.avaliacao.services.exceptions.DatabaseExceptionOwn;
 import com.attornatus.avaliacao.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class EnderecoService {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PessoaRepository repositoryPessoa;
 
@@ -35,28 +39,60 @@ public class EnderecoService {
 		Optional<Endereco> obj = enderecoRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	@Transactional
 	public Endereco insert(EnderecoForm form) {
 		Endereco obj = form.converter(repositoryPessoa);
-		
+
 		resetStatus(obj.getPessoa());
-		obj.setStatus(StatusEndereco.PRINCIPAL); 
-		
+		obj.setStatus(StatusEndereco.PRINCIPAL);
+
 		return enderecoRepository.save(obj);
 	}
-	
+
+	@Transactional
+	public void delete(Long id) {
+		if (enderecoRepository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			enderecoRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseExceptionOwn(e.getMessage());
+		}
+
+	}
+
+	@Transactional
+	public Endereco update(Long id, Endereco obj) {
+		try {
+			Endereco entity = enderecoRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return enderecoRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
 	private void resetStatus(Pessoa pessoa) {
 		List<Endereco> list = new ArrayList<>();
-		List<EnderecoDto> enderecos= new ArrayList<>();
+		List<EnderecoDto> enderecos = new ArrayList<>();
 		enderecos.addAll(pessoa.getEnderecos().stream().map(EnderecoDto::new).collect(Collectors.toList()));
 		enderecos.forEach(end -> {
 			Pessoa person = repositoryPessoa.findById(pessoa.getId())
 					.orElseThrow(() -> new ResourceNotFoundException(pessoa.getId()));
 			Endereco recupera = new Endereco(end.getId(), end.getLogradouro(), end.getCep(), end.getNumero(), person);
-			//recupera.setId(end.getId());
+			// recupera.setId(end.getId());
 			recupera.setStatus(StatusEndereco.CONTATO);
 			enderecoRepository.saveAndFlush(recupera);
 		});
+	}
+
+	private void updateData(Endereco entity, Endereco obj) {
+		entity.setCep(obj.getCep());
+		entity.setLogradouro(obj.getLogradouro());
+		entity.setNumero(obj.getNumero());
 	}
 }
