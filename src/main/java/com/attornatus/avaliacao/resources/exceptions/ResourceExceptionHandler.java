ee9a923b411ffffa5ece410ca60d1c887ac9237a
Flavio Rogerio <solebac@hotmail.com>
2023-01-13 00:38:27 -0300

    -implemente regra exception ResourceExceptionHandler com controle da anotação @ControllerAdvice

---
+++ src/main/java/com/attornatus/avaliacao/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.attornatus.avaliacao.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.attornatus.avaliacao.services.exceptions.DatabaseExceptionOwn;
+import com.attornatus.avaliacao.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), 
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	@ExceptionHandler(DatabaseExceptionOwn.class)
+	public ResponseEntity<StandardError> resourceDatabase(DatabaseExceptionOwn e, HttpServletRequest request) {
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), 
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
+
